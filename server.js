e50bd27bc062acb6aa80e83132230beb22d6ef82
Vikram <viksgit7@gmail.com>
2023-03-26 20:40:43 +0530

    added json server

--- server.js
+++ server.js
@@ -1,19 +1,53 @@
+require("dotenv").config()
 const express = require("express")
 const app = express();
 const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
-const posts=[{
-    "name":"vik"
-}]
+//posts
+const posts = [
+    {
+      username: 'vik',
+      title: 'Post 1'
+    },
+    {
+      username: 'elonmusk',
+      title: 'Post 2'
+    }
+  ]
 
-app.get("/posts",(req,res)=>{
-    res.json(posts)
+//getting posts only if logged in
+app.get("/posts",authenticateToken,(req,res)=>{
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
-app.post("./login",(req,res)=>{
+//logging in
+app.post("/login",(req,res)=>{
     //Authenticate user
+    const username = req.body.username;
+    const user = {name:username}
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken:accessToken})
+
 })
 
-app.listen(8080);
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    //If there is no token
+    if (token == null) {
+        return res.sendStatus(401)
+    }
+    else{
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            //If the token is invalid
+            if (err) return res.sendStatus(403)
+            //if Token is valid
+            req.user = user
+            next()
+        })
+    }
+  }
+
+app.listen(6080);
